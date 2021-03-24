a86e71d6e588eabba156cff907df63da2f95819f
Ark005 <005ark@gmail.com>
2021-03-24 16:49:54 +0300

    baner_size

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -206,3 +206,34 @@ THUMBNAIL_DEBUG = True,
 
 LANGUAGE_CODE = 'ru-RU'  # для русской локализации
 USE_I18N = True # интернационалицация по-умолчанию включена
+
+TINYMCE_DEFAULT_CONFIG = {
+'theme': "advanced", # default value
+'relative_urls': False, # default value
+'plugins': 'table,spellchecker,paste,searchreplace',
+'theme_advanced_buttons1': 'bold,italic,underline,bullist,numlist,link,unlink,styleselect,fontselect,fontsizeselect',
+'width': '100%',
+'height': 300,
+'paste_text_sticky': True,
+'paste_text_sticky_default': True,
+'valid_styles': 'font-weight,font-style,text-decoration',
+'fontsize_formats': "8pt 10pt 11pt 12pt 13pt 14pt 16pt 18pt 20pt 24pt 36pt",
+'font_formats': "Andale Mono=andale mono,times;" +
+    "Arial=arial,helvetica,sans-serif;" +
+    "Arial Black=arial black,avant garde;" +
+    "Book Antiqua=book antiqua,palatino;" +
+    "Comic Sans MS=comic sans ms,sans-serif;" +
+    "Courier New=courier new,courier;" +
+    "Georgia=georgia,palatino;" +
+    "Helvetica=helvetica;" +
+    "Impact=impact,chicago;" +
+    "Symbol=symbol;" +
+    "Tahoma=tahoma,arial,helvetica,sans-serif;" +
+    "Terminal=terminal,monaco;" +
+    "Times New Roman=times new roman,times;" +
+    "Trebuchet MS=trebuchet ms,geneva;" +
+    "Verdana=verdana,geneva;" +
+    "Webdings=webdings;" +
+    "Wingdings=wingdings,zapf dingbats",}
+TINYMCE_SPELLCHECKER = True
+TINYMCE_COMPRESSOR = True
