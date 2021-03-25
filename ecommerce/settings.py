dffc924c4cf2dd2cfd88fe15570265209058383c
Ark005 <005ark@gmail.com>
2021-03-25 11:07:37 +0300

    new google verification

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '#l+yarcxe!v^dhadywe4(s9!7jg$^^g5bp1fs-&hq#&2xz38n$'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
