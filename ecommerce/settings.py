ea02a6fe71d106c5d9feef912cb0eb9e621b6492
Ark005 <005ark@gmail.com>
2021-04-10 07:33:23 +0300

    + favicon

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'sorl.thumbnail',
+    'allauth.socialaccount.providers.vk',
   
   
     
@@ -70,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
   
 ]
 
